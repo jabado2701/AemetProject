4c430f9acfd33f0d82b464b3536a3eb48f5c6f5b
jabado2701 <jabado95@gmail.com>
2023-01-12 14:35:38 +0000

    Fourth commit

--- datamart-provider/src/main/java/org/example/MainDP.java
+++ datamart-provider/src/main/java/org/example/MainDP.java
@@ -1,6 +1,6 @@
 package org.example;
 
-public class Main {
+public class MainDP {
     public static void main(String[] args) {
         new DatamartController().startcallCenter();
 
--- temperature-service/src/main/java/org/example/MainTS.java
+++ temperature-service/src/main/java/org/example/MainTS.java
@@ -1,6 +1,6 @@
 package org.example;
 
-public class Main {
+public class MainTS {
     public static void main(String[] args) {
         new APICalls().gets();
     }
